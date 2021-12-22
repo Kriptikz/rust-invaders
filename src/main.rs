c6a0d24d22ea4557d1111a1b0c090142673501ba
Kriptikz <Kriptikz@users.noreply.github.com>
2021-12-22 11:13:38 -0600

    Setup initial window creation and position.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#![allow(unused)] // silence unused warning while learning
+
+use bevy::prelude::*;
+
+const PLAYER_SPRITE: &str = "player_a_02.png";
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor{
+            title: "Rust Invaders!".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut windows: ResMut<Windows>
+) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // position window
+    let mut window = windows.get_primary_mut().unwrap();
+    window.set_position(IVec2::new(1314, 0));
 }
