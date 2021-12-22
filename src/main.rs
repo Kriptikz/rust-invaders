61e71cb8236c9d47a467b55a01375df2f5487a85
Kriptikz <Kriptikz@users.noreply.github.com>
2021-12-22 12:01:14 -0600

    Renamed PlayerSpeed component to Speed so we can use by other entities Created a player_fire system for spawning the laser and adding components Added the player_fire system to the App

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ const TIME_STEP: f32 = 1.0 / 60.0;
 
 // Resources
 pub struct Materials {
-    player: Handle<ColorMaterial>, 
+    player: Handle<ColorMaterial>,
+    laser: Handle<ColorMaterial>,
 }
 struct WinSize {
     w: f32,
@@ -18,8 +19,9 @@ struct WinSize {
 
 // Components
 struct Player;
-struct PlayerSpeed(f32);
-impl Default for PlayerSpeed {
+struct Laser;
+struct Speed(f32);
+impl Default for Speed {
     fn default() -> Self {
         Self (500.0)
     }
@@ -38,6 +40,7 @@ fn main() {
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup_actors", SystemStage::single(player_spawn.system()))
         .add_system(player_movement.system())
+        .add_system(player_fire.system())
         .run();
 }
 
@@ -55,6 +58,7 @@ fn setup(
     // create the main resources
     commands.insert_resource(Materials {
         player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        laser: materials.add(asset_server.load(LASER_SPRITE).into()),
     });
     commands.insert_resource(WinSize {
         w: window.width(),
@@ -79,12 +83,12 @@ fn player_spawn(mut commands: Commands, materials: Res<Materials>, win_size: Res
             ..Default::default()
         })
         .insert(Player)
-        .insert(PlayerSpeed::default());
+        .insert(Speed::default());
 }
 
 fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&PlayerSpeed, &mut Transform, With<Player>)>
+    mut query: Query<(&Speed, &mut Transform, With<Player>)>
 ) {
     if let Ok((speed, mut transform, _)) = query.single_mut() {
         let dir = if keyboard_input.pressed(KeyCode::Left) {
@@ -95,5 +99,29 @@ fn player_movement(
             0.0
         };
         transform.translation.x += dir * speed.0 * TIME_STEP;
+    }
+}
+
+fn player_fire(
+    mut commands: Commands,
+    kb: Res<Input<KeyCode>>,
+    materials: Res<Materials>,
+    mut query: Query<(&Transform, With<Player>)>,
+) {
+    if let Ok((player_tf, _)) = query.single_mut() {
+        if kb.pressed(KeyCode::Space) {
+            let x = player_tf.translation.x;
+            let y = player_tf.translation.y;
+            commands.spawn_bundle(SpriteBundle {
+                material: materials.laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y, 0.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Laser)
+            .insert(Speed::default());
+        }
     }
 }
