c4edfbf974e1eccbdf1a6bfa3dc214d32e6a4571
Kriptikz <Kriptikz@users.noreply.github.com>
2021-12-22 12:14:00 -0600

    Adjusted player_fire laser spawn to be above the players sprite

--- src/main.rs
+++ src/main.rs
@@ -119,7 +119,7 @@ fn player_fire(
                 .spawn_bundle(SpriteBundle {
                     material: materials.laser.clone(),
                     transform: Transform {
-                        translation: Vec3::new(x, y, 0.0),
+                        translation: Vec3::new(x, y + 15.0, 0.0),
                         ..Default::default()
                     },
                     ..Default::default()
