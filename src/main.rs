7e707db3083eac3aa5ba455d5eaff886574702af
Kriptikz <Kriptikz@users.noreply.github.com>
2021-12-22 14:57:53 -0600

    Added Resources and Components for enemies Added the EnemyPlugin to the App Created the laser_hit_enemy collision system Added the laser_hit_enemy system to the App

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-#![allow(unused)] // silence unused warning while learning
+//#![allow(unused)] // silence unused warning while learning
 
 mod player;
+mod enemy;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::collide_aabb::collide};
 use player::PlayerPlugin;
+use enemy::EnemyPlugin;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
+const ENEMY_SPRITE: &str = "enemy_a_01.png";
 const SCALE: f32 = 0.5;
 const TIME_STEP: f32 = 1.0 / 60.0;
 
@@ -15,16 +18,22 @@ const TIME_STEP: f32 = 1.0 / 60.0;
 pub struct Materials {
     player: Handle<ColorMaterial>,
     laser: Handle<ColorMaterial>,
+    enemy: Handle<ColorMaterial>,
 }
 struct WinSize {
+    #[allow(unused)]
     w: f32,
     h: f32,
 }
+
+struct ActiveEnemies(u32);
 
 // Components
 struct Player;
 struct PlayerReadyFire(bool);
 struct Laser;
+
+struct Enemy;
 struct Speed(f32);
 impl Default for Speed {
     fn default() -> Self {
@@ -41,9 +50,12 @@ fn main() {
             height: 676.0,
             ..Default::default()
         })
+        .insert_resource(ActiveEnemies(0))
         .add_plugins(DefaultPlugins)
         .add_plugin(PlayerPlugin)
+        .add_plugin(EnemyPlugin)
         .add_startup_system(setup.system())
+        .add_system(laser_hit_enemy.system())
         .run();
 }
 
@@ -53,7 +65,7 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>
 ) {
-    let mut window = windows.get_primary_mut().unwrap();
+    let window = windows.get_primary_mut().unwrap();
 
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -62,6 +74,7 @@ fn setup(
     commands.insert_resource(Materials {
         player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
         laser: materials.add(asset_server.load(LASER_SPRITE).into()),
+        enemy: materials.add(asset_server.load(ENEMY_SPRITE).into()),
     });
     commands.insert_resource(WinSize {
         w: window.width(),
@@ -70,4 +83,34 @@ fn setup(
 
     // position window
     window.set_position(IVec2::new(1314, 0));
+}
+
+fn laser_hit_enemy(
+    mut commands: Commands,
+    mut laser_query: Query<(Entity, &Transform, &Sprite, With<Laser>)>,
+    mut enemy_query: Query<(Entity, &Transform, &Sprite, With<Enemy>)>,
+    mut active_enemies: ResMut<ActiveEnemies>
+) {
+    for (laser_entity, laser_tf, laser_sprite, _) in laser_query.iter_mut() {
+        for (enemy_entity, enemy_tf, enemy_sprite, _) in enemy_query.iter_mut() {
+            let laser_scale = Vec2::from(laser_tf.scale);
+            let enemy_scale = Vec2::from(enemy_tf.scale);
+
+            let collision = collide(
+                laser_tf.translation,
+                laser_sprite.size * laser_scale,
+                enemy_tf.translation,
+                enemy_sprite.size * enemy_scale,
+            );
+
+            if let Some(_) = collision {
+                // remove the enemy
+                commands.entity(enemy_entity).despawn();
+                active_enemies.0 -= 1;
+
+                // remove the laser
+                commands.entity(laser_entity).despawn();
+            }
+        }
+    }
 }
