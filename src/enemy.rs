2b1569b9def584542fff1f5c57374d4c86d93469
Kriptikz <Kriptikz@users.noreply.github.com>
2021-12-24 14:46:14 -0600

    Added enemy movement and formations

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,39 +1,112 @@
+use std::f32::consts::PI;
+
 use bevy::{core::FixedTimestep, prelude::*};
 use rand::{thread_rng, Rng};
 
-use crate::{ActiveEnemies, WinSize, Materials, SCALE, Enemy, Laser, FromEnemy, Speed, TIME_STEP};
+use crate::{ActiveEnemies, WinSize, Materials, SCALE, Enemy, Laser, FromEnemy, Speed, TIME_STEP, MAX_ENEMIES, MAX_FORMATION_MEMBERS};
+
+// Formation
+
+// Component
+#[derive(Default, Clone)]
+struct Formation {
+    start: (f32, f32),
+    radius: (f32, f32),
+    offset: (f32, f32),
+    angle: f32,
+    group_id: u32,
+}
+
+// Resource
+#[derive(Default)]
+struct FormationMaker {
+    group_seq: u32,
+    current_formation: Option<Formation>,
+    current_formation_members: u32,
+}
+
+impl FormationMaker {
+    fn make(&mut self, win_size: &WinSize) -> Formation {
+        match (
+            &self.current_formation,
+            self.current_formation_members >= MAX_FORMATION_MEMBERS
+        ) {
+            // If first formation or previous formation full
+            (None, _) | (_, true) => {
+                // Compute the start x/y
+                let mut rng = thread_rng();
+                let h_span = win_size.h / 2.0 - 100.0;
+                let w_span = win_size.w / 4.0;
+                let x = if rng.gen::<bool>() {
+                    win_size.w
+                } else {
+                    -win_size.w
+                };
+                let y = rng.gen_range(-h_span..h_span) as f32;
+                let start = (x, y);
+
+                // Compute offset and radius
+                let offset = (rng.gen_range(-w_span..w_span), rng.gen_range(0.0..h_span));
+                let radius = (rng.gen_range(80.0..150.0), 100.0);
+                let angle: f32 = (y - offset.0).atan2(x - offset.1);
+
+                // Create new formation
+                self.group_seq += 1;
+                let group_id = self.group_seq;
+                let formation = Formation {
+                    start,
+                    offset,
+                    radius,
+                    angle,
+                    group_id,
+                };
+
+                // Close, set, and return
+                self.current_formation = Some(formation.clone());
+                self.current_formation_members = 1;
+                formation
+            }
+            // If still within the formation cound
+            (Some(tmpl), false) => {
+                self.current_formation_members += 1;
+                tmpl.clone()
+            }
+        }
+    }
+}
+
 
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut bevy::prelude::AppBuilder){
         app
-        .add_system(enemy_laser_movement.system())
-        .add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(1.0))
-                .with_system(enemy_spawn.system())
-        ).add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(0.9))
-                .with_system(enemy_fire.system()),
-        );
+            .insert_resource(FormationMaker::default())
+            .add_system(enemy_laser_movement.system())
+            .add_system(enemy_movement.system())
+            .add_system_set(
+                SystemSet::new()
+                    .with_run_criteria(FixedTimestep::step(1.0))
+                    .with_system(enemy_spawn.system())
+            ).add_system_set(
+                SystemSet::new()
+                    .with_run_criteria(FixedTimestep::step(0.9))
+                    .with_system(enemy_fire.system()),
+            );
     }
 }
 
 fn enemy_spawn(
     mut commands: Commands,
     mut active_enemies: ResMut<ActiveEnemies>,
+    mut formation_maker: ResMut<FormationMaker>,
     win_size: Res<WinSize>,
     materials: Res<Materials>
 ) {
-    if active_enemies.0 < 1 {
-        //compute random position
-        let mut rng = thread_rng();
-        let w_span = win_size.w / 2.0 - 100.0;
-        let h_span = win_size.h / 2.0 - 100.0;
-        let x = rng.gen_range(-w_span..w_span) as f32;
-        let y = rng.gen_range(-h_span..h_span) as f32;
+    if active_enemies.0 < MAX_ENEMIES {
+        // get the formation and start x/y
+        let formation = formation_maker.make(&win_size);
+        let (x, y) = formation.start;
 
         // spawn enemy
         commands
@@ -46,9 +119,62 @@ fn enemy_spawn(
             },
             ..Default::default()
         })
-        .insert(Enemy);
+        .insert(Enemy)
+        .insert(Speed::default())
+        .insert(formation);
 
         active_enemies.0 += 1;
+    }
+}
+
+fn enemy_movement(
+    mut query: Query<(&mut Transform, &Speed, &mut Formation), With<Enemy>>
+) {
+
+    // for each enemy
+    for (mut tf, speed, mut formation) in query.iter_mut() {
+        let max_distance = TIME_STEP * speed.0;
+        let x_org = tf.translation.x;
+        let y_org = tf.translation.y;
+
+        // Get the ellipse
+        let (x_offset, y_offset) = formation.offset;
+        let (x_radius, y_radius) = formation.radius;
+
+        // Compute the next angle
+        let dir = if formation.start.0 > 0.0 { 1.0 } else { -1.0 };
+        let angle = formation.angle + dir * speed.0 * TIME_STEP / (x_radius.min(y_radius) * PI / 2.0);
+
+        // Calculate the destination
+        let x_dst = x_radius * angle.cos() + x_offset;
+        let y_dst = y_radius * angle.sin() + y_offset;
+
+        // Calculate distance
+        let dx = x_org - x_dst;
+        let dy = y_org - y_dst;
+
+        let distance = (dx * dx + dy * dy).sqrt();
+        let distance_ratio = if distance == 0.0 {
+            0.0
+        } else {
+            max_distance / distance
+        };
+
+        // Calculate the final x/y (make sure to not overshoot)
+        let x = x_org - dx * distance_ratio;
+        let x = if dx > 0.0 { x.max(x_dst) } else { x.min(x_dst) };
+        let y = y_org - dy * distance_ratio;
+        let y = if dy > 0.0 { y.max(y_dst) } else { y.min(y_dst) };
+
+        // Start rotating the formation angle only when sprite are on or close to desination
+        if distance < max_distance * speed.0 / 20.0 {
+            formation.angle = angle;
+        }
+
+        // Apply the transformation
+        tf.translation.x = x;
+        tf.translation.y = y;
+
     }
 }
 
